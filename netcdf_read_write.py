163b34df946ad0638326a62e3c25089cc2c7daa3
Duncan Barlow <duncan.barlow@u-bordeaux.fr>
2022-04-08 11:46:59 +0200

    Rotate quad spot to make cone

--- netcdf_read_write.py
+++ netcdf_read_write.py
@@ -1,9 +1,11 @@
 from netCDF4 import Dataset
 import numpy as np
 from os import path
+import os
 import healpy as hp
 import glob
 
+small_num = 1.0e-10
 
 
 def read_intensity(data_location):
@@ -26,7 +28,7 @@ def rotate_cone_and_save(the_data, quad_name, hpxmap, imap_nside, pointing_theta
     quad_slice = slice(quad_start_ind, quad_start_ind+4)
     old_imap_theta = np.mean(the_data['Theta'][quad_slice])
     old_imap_phi = np.mean(the_data['Phi'][quad_slice])
-    
+
     cone_name = the_data['Cone'][quad_slice]
     cone_name = cone_name[0]
     quad_name = the_data['Quad'][quad_slice]
@@ -38,7 +40,7 @@ def rotate_cone_and_save(the_data, quad_name, hpxmap, imap_nside, pointing_theta
 
     cone_name = str(cone_name)
     cone_name = cone_name.replace(".", "_")
-    
+
     imap_npix = imap_nside**2 * 12
     index = np.arange(imap_npix)
     imap_theta, imap_phi = hp.pix2ang(imap_nside, index)
@@ -66,11 +68,33 @@ def rotate_cone_and_save(the_data, quad_name, hpxmap, imap_nside, pointing_theta
         rotate_phi = - old_imap_phi + np.mean(the_data['Phi'][quad_slice])
         imap_phi_new = imap_phi - np.radians(rotate_phi)
 
+        ############## new ##################
+        theta = np.radians(old_imap_theta)
+        phi = np.radians(old_imap_phi)
+        alpha = np.radians(old_imap_theta + rotate_theta)
+        beta = np.radians(old_imap_phi - rotate_phi)
+
+        rotation_matrix = np.matmul(np.matmul(rot_mat(beta, "z"), rot_mat(alpha, "y")),
+                                    np.matmul(rot_mat(-theta, "y"), rot_mat(-phi, "z")))
+        r = 1.0
+        x_o = r * np.cos(imap_phi) * np.sin(imap_theta)
+        y_o = r * np.sin(imap_phi) * np.sin(imap_theta)
+        z_o = r * np.cos(imap_theta)
+
+        x_n = rotation_matrix[0,0] * x_o + rotation_matrix[0,1] * y_o + rotation_matrix[0,2] * z_o
+        y_n = rotation_matrix[1,0] * x_o + rotation_matrix[1,1] * y_o + rotation_matrix[1,2] * z_o
+        z_n = rotation_matrix[2,0] * x_o + rotation_matrix[2,1] * y_o + rotation_matrix[2,2] * z_o
+
+        imap_theta_new = np.arccos(z_n)
+        imap_phi_new = np.arctan2(y_n, x_n)
+        ######################################
+
         quad_pointing = quad_grp.createVariable('quad_pointing', 'f4', ('pointing_dim',))
         quad_pointing[0] = the_data['target_radius']
         quad_pointing[1] = pointing_theta + np.radians(rotate_theta)
         quad_pointing[2] = pointing_phi + np.radians(rotate_phi)
 
+        # Rotation in theta is not correct for large theta (roughly true for small theta)
         intensity_map_rotate = hp.get_interp_val(hpxmap, imap_theta_new, imap_phi_new)
 
         quad_grp.createDimension('intensity_dim', imap_npix)
@@ -79,3 +103,40 @@ def rotate_cone_and_save(the_data, quad_name, hpxmap, imap_nside, pointing_theta
         intensity_map[:] = intensity_map_rotate[:]
 
     rootgrp.close()
+
+
+
+def rot_mat(theta, axis):
+    mat = np.zeros([3,3])
+    sin_t = np.sin(theta)
+    cos_t = np.cos(theta)
+
+    if (abs(sin_t) < small_num):
+        sin_t = 0.0
+        cos_t = 1.0
+    if (abs(cos_t) < small_num):
+        cos_t = 0.0
+        sin_t = 1.0
+
+    if (axis == "x"):
+        mat[0,0] = 1.0
+        mat[1,1] = cos_t
+        mat[2,2] = cos_t
+        mat[1,2] = -sin_t
+        mat[2,1] = sin_t
+    elif (axis == "y"):
+        mat[0,0] = cos_t
+        mat[1,1] = 1.0
+        mat[2,2] = cos_t
+        mat[0,2] = sin_t
+        mat[2,0] = -sin_t
+    elif (axis == "z"):
+        mat[0,0] = cos_t
+        mat[1,1] = cos_t
+        mat[2,2] = 1.0
+        mat[0,1] = -sin_t
+        mat[1,0] = sin_t
+    else:
+        print("Invalid parameter 'axis' try setting string 'x', 'y', or 'z'")
+
+    return mat
