d6dcd1f54b008328e72d5ff9f0418a94fb50f147
Duncan Barlow <duncan.barlow@u-bordeaux.fr>
2022-04-28 15:43:27 +0200

    Add plot of test and train accuracy

---
+++ nn_plots.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+
+global figure_location
+figure_location = "plots"
+
+def plotting(epochs, costs, train_acc, test_acc, epoch_count, lr):
+    # Plot the cost
+    fig = plt.figure()
+    ax1 = plt.axes()
+
+    ax2 = ax1.twinx()
+    ax2.tick_params(axis='y', labelcolor = 'tab:red')
+
+    ax1.plot(epochs, costs)
+    ax1.set_xticks(range(0, epoch_count+int(epoch_count/10), int(epoch_count/10)))
+    ax1.set_ylabel('cost')
+    ax1.set_xlabel('epochs')
+    ax1.set_title("Learning rate =" + str(lr))
+
+    ax2.plot(epochs, train_acc, "r", linestyle="-")
+    ax2.plot(epochs, test_acc, "r", linestyle=":")
+    ax2.set_ylabel('Mean absolute error')
+
+    plt.savefig(figure_location+"/cost_over_epochs.png", dpi=300, bbox_inches='tight')
