56955660d084f3401bc410219daeda06f23ee6d3
Duncan Barlow <duncan.barlow@u-bordeaux.fr>
2024-01-04 15:58:09 +0100

    Fix units and format in print statement

--- netcdf_read_write.py
+++ netcdf_read_write.py
@@ -127,8 +127,8 @@ def retrieve_xtrain_and_delete(min_parallel, max_parallel, dataset, dataset_para
                 dataset["rms"][iex,tind] = uim.alms2rms(dataset["real_modes"][iex,tind,:], dataset["imag_modes"][iex,tind,:], dataset_params["LMAX"])
 
                 print("With density profiles:")
-                print('Intensity per steradian, {:.2e}W/sr^-1'.format(dataset["avg_flux"][iex, tind]))
-                print("The rms is: ", dataset["rms"][iex,tind]*100.0, "%")
+                print('Mean ablation pressure: {:.2f}Mbar'.format(dataset["avg_flux"][iex, tind]))
+                print("The rms is: {:.2f} %".format(dataset["rms"][iex,0]*100.0))
 
             else:
                 dir_illumination = run_location + "/" + sys_params["ifriit_ouput_name"]
@@ -141,8 +141,8 @@ def retrieve_xtrain_and_delete(min_parallel, max_parallel, dataset, dataset_para
                     dataset["rms"][iex,tind] = uim.alms2rms(dataset["real_modes"][iex,tind,:], dataset["imag_modes"][iex,tind,:], dataset_params["LMAX"])
 
                     print("Without density profiles:")
-                    print('Intensity per steradian, {:.2e}W/sr^-1'.format(dataset["avg_flux"][iex, 0]))
-                    print("The rms is: ", dataset["rms"][iex,0]*100.0, "%")
+                    print('Intensity per steradian, {:.2e}W/sr'.format(dataset["avg_flux"][iex, 0]))
+                    print("The rms is: {:.2f} %".format(dataset["rms"][iex,0]*100.0))
                 else:
                     print("Broken illumination! Probably due to CBET convergence?")
 
